0fa7ad21d692b0a13d4d11921069b07e17559e6d
cchao595 <cchao595@gmail.com>
2016-11-11 13:59:55 -0500

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -193,7 +193,7 @@ def userprofiles():
   cursor.close()
   for user in userids:
     cmd = "SELECT U.username, U.dob, U.email FROM GeneralUsers AS G, Users AS U WHERE U.user_id = :name1"
-    cmd2 = "SELECT P.title UNIQUE FROM PersonalPlaylists_manages AS P WHERE P.user_id = :name1"
+    cmd2 = "SELECT DISTINCT P.title FROM PersonalPlaylists_manages AS P WHERE P.user_id = :name1"
     cursor2 = g.conn.execute(text(cmd), name1 = user)
     cursor3 = g.conn.execute(text(cmd2), name1 = user)
   #cursor = g.conn.execute("SELECT U.username, U.dob, U.email FROM GeneralUsers AS G, Users AS U WHERE U.user_id = G.user_id")
