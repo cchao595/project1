9fd0f02c7ec7795fb1d1d37e377b9a6e7acf9c6a
cchao595 <cchao595@gmail.com>
2016-11-12 02:50:55 -0500

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -206,8 +206,9 @@ def userprofiles():
       follow = []
       for w in item1:
         follow.append(w)
-      str2 += ', '.join(str(e) for e in follow)
-      infoperuser.append(str2)
+      str2 += ' '.join(str(e) for e in follow) + ', '
+    
+    infoperuser.append(str2)
     cursor.close()
     row2 = cursor3.fetchall()
     infoperuser.append('Playlists')
