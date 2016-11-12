34ae55aaf9ec2df9836ffbaa76d0ee12cbe4def4
cchao595 <cchao595@gmail.com>
2016-11-12 01:43:34 -0500

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -240,12 +240,15 @@ def artists():
       for x in item:
         artistinfo.append(x)
       str1 = ' '.join(str(e) for e in artistinfo)
-      infoperartist.append(artistinfo)
+      infoperartist.append(str1)
     row2 = cursor3.fetchall()
     infoperartist.append('Albums')
-    for item2 in row2:     
-      for x in item2:
-        infoperartist.append(x)
+    for item2 in row2:
+      albuminfo = []     
+      for y in item2:
+        albuminfo.append(x)
+      str2 = ' '.join(str(e) for e in albuminfo)
+      infoperartist.append(str1)
 
   cursor3.close()
   cursor2.close()
