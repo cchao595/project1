08fe97eda3da1618c4208fc5baeaba8974487876
cchao595 <cchao595@gmail.com>
2016-11-12 03:03:51 -0500

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -133,11 +133,13 @@ def index():
   #
   # example of a database query
   #
-  cursor = g.conn.execute("SELECT a.name FROM Follows AS f, Artists AS a WHERE f.artist_id = a.artist_id GROUP BY f.artist_id, a.name ORDER BY count(*) desc LIMIT 5;")
+  cursor = g.conn.execute("SELECT a.name FROM Follows AS f, Artists AS a WHERE f.artist_id = a.artist_id GROUP BY f.artist_id, a.name ORDER BY count(*) desc LIMIT 5")
   names = []
   for result in cursor:
     names.append(result['name'])  # can also be accessed using result[0]
   cursor.close()
+
+  cursor = g.conn.execute("SELECT s.title FROM Listens as l, Songs AS s WHERE l.song_id = s.song_id GROUP BY l.song_id, s.title ORDER BY count(*) desc LIMIT 5")
 
   #
   # Flask uses Jinja templates, which is an extension to HTML where you can
