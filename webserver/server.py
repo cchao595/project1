8c39de2a026f790c1fd589831074cb70d3b719b9
cchao595 <cchao595@gmail.com>
2016-11-12 01:41:11 -0500

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -230,7 +230,7 @@ def artists():
     artists.append(result['artist_id'])  # can also be accessed using result[0]
   cursor.close()
   for artist in artists:
-    cmd = "SELECT A.name, A.genre FROM artists AS A WHERE A.artist_id = :name1 GROUP BY A.genre"
+    cmd = "SELECT A.name, A.genre FROM artists AS A WHERE A.artist_id = :name1"
     cmd2 = "SELECT B.name, B.year, B.no_of_songs FROM albums as B, affiliated as A, studio as S, produces as P WHERE A.artist_id = :name1 and S.studio_id = P.studio_id and P.album_id = B.album_id"
     cursor2 = g.conn.execute(text(cmd), name1 = artist)
     cursor3 = g.conn.execute(text(cmd2), name1 = artist)
