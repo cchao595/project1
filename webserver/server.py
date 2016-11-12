80249a2f999aa79a837d2c35f776098148a9222d
cchao595 <cchao595@gmail.com>
2016-11-12 02:43:09 -0500

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -200,7 +200,7 @@ def userprofiles():
     row = cursor2.fetchone()
     for item in row:
       infoperuser.append(item)
-    row1 = cursor.fetchone()
+    row1 = cursor.fetchall()
     for item1 in row1:
       follow = []
       for w in item1:
