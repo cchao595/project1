0144de4881851869ceab13460f1ade73fe64489b
cchao595 <cchao595@gmail.com>
2016-11-16 18:54:33 -0500

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -280,36 +280,31 @@ def gandm():
 @app.route('/', methods =['GET', 'POST'])
 def songs_given_playlist_id():
   pp_name = request.form['title']
-  playlistinfo = []
-  try:
-    cmd = "SELECT P.title, P.description FROM publicplaylists_generates AS P WHERE P.title LIKE :x"
-    cursor = g.conn.execute(text(cmd), name1 = '%' + pp_name + '%')
-  except:
-    return redirect('/invalid_action/')
+  return redirect('search_results', query=pp_name)
 
+@app.route('/search_results/<query>')
+def playlistsearch_results(query):
+  playlistinfo = []
+  cmd = "SELECT P.title, P.description FROM publicplaylists_generates AS P WHERE P.title LIKE :x"
   row = cursor.fetchall()
-  cursor.close()
-  # print gm title and description
   for item in row:
     ndinfo = []
     for x in item:
-      ppinfo.append(x)
+      ndinfo.append(x)
     str1 = ' - '.join(str(e) for e in ndinfo)
     playlistinfo.append(str1)
     playlistinfo.append('Songs: Title - Artist - Length(s) - Explicit')    
-  try:
-    cmd = "SELECT S.title, A.name, S.song_length/1000 as length, S.explicit FROM songs AS S, artists AS A, PublicPlaylists_Generates AS P, records AS R WHERE p.title = :title and P.song_id = S.song_id and P.song_id = R.song_id and A.artist_id = R.artist_id"
-    cursor = g.conn.execute(text(cmd), title = pp_name)
-  except:
-    return redirect('/invalid_action/')  
-  row = cursor.fetchall()
-  for item in row:
-    songinfo = []
-    for y in item:
-      songinfo.append(y)
-    str3 = ' - '.join(str(e) for e in songinfo)
-    playlistinfo.append(str3)
-    cursor.close()
+    cmd2 = "SELECT S.title, A.name, S.song_length/1000 as length, S.explicit FROM songs AS S, artists AS A, PublicPlaylists_Generates AS P, records AS R WHERE p.title = :title and P.song_id = S.song_id and P.song_id = R.song_id and A.artist_id = R.artist_id"
+    cursor2 = g.conn.execute(text(cmd), title = pp_name) 
+    row = cursor2.fetchall()
+    for item in row:
+      songinfo = []
+      for y in item:
+        songinfo.append(y)
+      str3 = ' - '.join(str(e) for e in songinfo)
+      playlistinfo.append(str3)
+    cursor2.close()
+  cursor1.close()
         
   context = dict(data = playlistinfo)
   return render_template("lookup_playlist.html", **context)
