59504031fe3229f013d01e7b42c4578474973a21
cchao595 <cchao595@gmail.com>
2016-11-16 18:44:44 -0500

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -18,7 +18,6 @@ import os
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
 from flask import Flask, request, render_template, g, redirect, Response
-from spotify_queries.py import *
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
