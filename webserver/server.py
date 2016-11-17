0352dc7dba70fbf0d034e0f2136ef962bf07ba9b
cchao595 <cchao595@gmail.com>
2016-11-16 19:36:06 -0500

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -281,8 +281,34 @@ def gandm():
 # userinput: publicplaylist_id ex. 0rk49r
 @app.route('/', methods =['GET', 'POST'])
 def songs_given_playlist_id():
-  pp_name = request.form['title']
-  return redirect(url_for('search_results', query=pp_name))
+  if request.method == 'POST':
+    pp_name = request.form['title']
+    playlistinfo = []
+    cmd = "SELECT P.title, P.description FROM publicplaylists_generates AS P WHERE P.title LIKE :x"
+    cursor = g.conn.execute(text(cmd), title = pp_name)
+    row = cursor.fetchall()
+    for item in row:
+      ndinfo = []
+      for x in item:
+        ndinfo.append(x)
+      str1 = ' - '.join(str(e) for e in ndinfo)
+      playlistinfo.append(str1)
+      playlistinfo.append('Songs: Title - Artist - Length(s) - Explicit')    
+      cmd2 = "SELECT S.title, A.name, S.song_length/1000 as length, S.explicit FROM songs AS S, artists AS A, PublicPlaylists_Generates AS P, records AS R WHERE p.title = :title and P.song_id = S.song_id and P.song_id = R.song_id and A.artist_id = R.artist_id"
+      cursor2 = g.conn.execute(text(cmd2), title = query) 
+      row = cursor2.fetchall()
+      for item in row:
+        songinfo = []
+        for y in item:
+          songinfo.append(y)
+        str3 = ' - '.join(str(e) for e in songinfo)
+        playlistinfo.append(str3)
+      cursor2.close()
+    cursor.close()
+    context = dict(data = playlistinfo)
+    return render_template("search_results.html", **context)
+  else:
+    return render_template("search_reseults.html")
 
 @app.route('/search_results', methods = ['GET', 'POST'])
 def playlistsearch_results(query):
